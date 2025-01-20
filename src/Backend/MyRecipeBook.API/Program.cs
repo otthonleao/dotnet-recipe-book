28c41491370db8e5eb3eb6a1556924987cd86f72
Otthon Leão <otthonleao@hotmail.com>
2025-01-20 18:37:43 -0400

    test: add Success() to RegisterUser with return 201 status code

--- src/Backend/MyRecipeBook.API/Program.cs
+++ src/Backend/MyRecipeBook.API/Program.cs
@@ -45,3 +45,7 @@ void MigrateDatabase()
     DatabaseMigration.Migrate(databaseType, connectionString, serviceScope.ServiceProvider);
 }
 
+public partial class Program
+{
+    protected Program() { }
+}
---
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MyRecipeBook.API;
+
+namespace WebApi.Test.User.Register;
+
+public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _httpClient;
+    
+    public RegisterUserTest(WebApplicationFactory<Program> factory) => _httpClient = factory.CreateClient();
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var response = await _httpClient.PostAsJsonAsync("api/User", request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+        responseData.RootElement.GetProperty("name").GetString().Should().NotBeNullOrWhiteSpace().And.Be(request.Name);
+    }
+
+}
