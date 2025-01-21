e56bf7e5cd5084b79fc5f6fc5c9f9701c2dda2c3
Otthon Leão <otthonleao@hotmail.com>
2025-01-21 00:54:25 -0400

    test: integration Error_Empty_Name() with test for each language messages errors

--- tests/WebApi.Test/User/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
@@ -5,6 +6,8 @@ using CommonTestUtilities.Requests;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using MyRecipeBook.API;
+using MyRecipeBook.Exceptions;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test.User.Register;
 
@@ -26,6 +29,33 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 
         var responseData = await JsonDocument.ParseAsync(responseBody);
         responseData.RootElement.GetProperty("name").GetString().Should().NotBeNullOrWhiteSpace().And.Be(request.Name);
+    }
+    
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Empty_Name(string culture)
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+        
+        if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
+            _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
+        _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+        
+        var response = await _httpClient.PostAsJsonAsync("api/User", request);
+        
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage =
+            ResourcesMessagesExceptions.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
+        errors.Should().ContainSingle().And.Contain(error => error.GetString()!.Equals(expectedMessage));
+
+
+
     }
 
 }
